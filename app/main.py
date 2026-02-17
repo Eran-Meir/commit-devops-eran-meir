7241e2ccb87939bb628aac668cfb5aa04b36959a
Eran Meir <Eran Meir>
2026-02-17 17:04:00 +0200

    Setting up the app, Dockerfile and requirements

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World from Comm-IT DevOps Assignment!'
+
+@app.route('/health')
+def health():
+    return 'OK', 200
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
