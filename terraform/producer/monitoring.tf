f91084fd886a9088bf6e77d3ae70b2320ab8e56d
Eran Meir <Eran Meir>
2026-02-21 17:34:07 +0200

    Setting up a dashboard for each VPC

--- terraform/producer/monitoring.tf
+++ terraform/producer/monitoring.tf
@@ -1,4 +1,4 @@
-resource "google_monitoring_dashboard" "commit_dashboard" {
-  dashboard_json = file("${path.module}/dashboard_e2e_overview.json")
+resource "google_monitoring_dashboard" "compute_dashboard" {
+  dashboard_json = file("${path.module}/dashboard_vpc_b_compute.json")
   project        = "commit-gcp-psc-eran-meir"
 }
