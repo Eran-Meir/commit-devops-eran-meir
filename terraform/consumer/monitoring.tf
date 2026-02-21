aabdf800178ba567adb3010bdeb1b98241298355
Eran Meir <Eran Meir>
2026-02-21 17:46:49 +0200

    Commenting out the dashboards to test if they reappear upon deployments

--- terraform/consumer/monitoring.tf
+++ terraform/consumer/monitoring.tf
@@ -1,4 +1,4 @@
-resource "google_monitoring_dashboard" "edge_dashboard" {
-  dashboard_json = file("${path.module}/dashboard_vpc_a_edge.json")
-  project        = "commit-gcp-psc-eran-meir"
-}
+# resource "google_monitoring_dashboard" "edge_dashboard" {
+#   dashboard_json = file("${path.module}/dashboard_vpc_a_edge.json")
+#   project        = "commit-gcp-psc-eran-meir"
+# }
--- terraform/producer/monitoring.tf
+++ terraform/producer/monitoring.tf
@@ -1,4 +1,4 @@
-resource "google_monitoring_dashboard" "compute_dashboard" {
-  dashboard_json = file("${path.module}/dashboard_vpc_b_compute.json")
-  project        = "commit-gcp-psc-eran-meir"
-}
+# resource "google_monitoring_dashboard" "compute_dashboard" {
+#   dashboard_json = file("${path.module}/dashboard_vpc_b_compute.json")
+#   project        = "commit-gcp-psc-eran-meir"
+# }
